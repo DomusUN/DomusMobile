cc69b2bd274cda769f8631a17d985af55a7d05e1
Juan Pablo Carmona <jucarmonam@unal.edu.co>
2022-10-18 23:07:19 -0500

    primer avance...

--- app/src/main/java/co/domus/domusmobile/MainActivity.kt
+++ app/src/main/java/co/domus/domusmobile/MainActivity.kt
@@ -3,24 +3,21 @@ package co.domus.domusmobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.ParagraphStyle
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import co.domus.domusmobile.ui.Chartreuse
 import co.domus.domusmobile.ui.DomusTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +30,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    HomeTitle()
+                    Column{
+                        HomeTitle()
+                        HomeButton()
+                    }
                 }
             }
         }
@@ -42,32 +42,61 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeTitle() {
-    Surface(color = MaterialTheme.colors.primary) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .background(
+                MaterialTheme.colors.primary
+            )
+    ) {
         Text(
-            buildAnnotatedString {
-                withStyle(style = ParagraphStyle(lineHeight = 30.sp)) {
-                    withStyle(style = SpanStyle(color = Chartreuse)) {
-                        append("Te damos la\n")
-                    }
-                    withStyle(
-                        style = SpanStyle(
-                            color = Chartreuse
-                        )
-                    ) {
-                        append("bienvenida a\n")
-                    }
-                    withStyle(
-                    style = SpanStyle(
-                        fontWeight = FontWeight.Bold,
-                        color = Chartreuse,
-                        fontSize = 24.sp
-                        )
-                    ){
-                        append("DOMUS")
-                    }
-                }
-            }
+            text = "Te damos la bienvenida a",
+            color = MaterialTheme.colors.onSurface,
+            maxLines = 3,
+            fontSize = 35.sp,
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(horizontal = 40.dp)
         )
+        Text(
+            text = "Domus",
+            color = MaterialTheme.colors.onSurface,
+            maxLines = 3,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(vertical = 40.dp, horizontal = 40.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.homeimg),
+            contentDescription = "",
+            modifier = Modifier.align(Alignment.BottomEnd).padding(top = 100.dp)
+        )
+    }
+}
+
+@Composable
+fun HomeButton() {
+    Column(
+        modifier = Modifier
+            .padding(top = 50.dp)
+            .padding(horizontal = 50.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(
+                MaterialTheme.colors.onSurface
+            ),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Los servicios que necesitas al alcance de tu mano")
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Ingresa")
+        }
+        Text("No tienes cuenta?")
+        Text("Registrate aqui")
     }
 }
 
@@ -75,6 +104,9 @@ fun HomeTitle() {
 @Composable
 fun DefaultPreview() {
     DomusTheme {
-        HomeTitle()
+        Column{
+            HomeTitle()
+            HomeButton()
+        }
     }
 }
