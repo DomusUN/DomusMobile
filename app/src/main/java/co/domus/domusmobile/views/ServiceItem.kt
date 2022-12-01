878bb566947fbb300295a36d2c068686ef9b324b
Juan Pablo Carmona <jucarmonam@unal.edu.co>
2022-12-01 01:16:44 -0500

    avances home

--- app/src/main/java/co/domus/domusmobile/views/ServiceItem.kt
+++ app/src/main/java/co/domus/domusmobile/views/ServiceItem.kt
@@ -3,62 +3,62 @@ package co.domus.domusmobile.views
 import android.annotation.SuppressLint
 import android.content.Context
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import co.domus.domusmobile.R
 import co.domus.domusmobile.model.Service
 
 @SuppressLint("DiscouragedApi")
 @Composable
 fun ServiceItem(service: Service, context: Context) {
+    val customCardElevation = CardDefaults.cardElevation(
+        defaultElevation = 5. dp
+    )
     Card(
         modifier = Modifier
-            .padding(8.dp, 4.dp)
+            .padding(20.dp, 10.dp)
             .fillMaxWidth()
-            .height(110.dp),
-        shape = RoundedCornerShape(8.dp)
+            .height(200.dp),
+        shape = RoundedCornerShape(8.dp),
+        elevation = customCardElevation,
+        colors = CardDefaults.cardColors(
+            containerColor =  MaterialTheme.colorScheme.primaryContainer,
+        ),
     ) {
-        Surface {
-            Row(
-                Modifier
-                    .padding(4.dp)
-                    .fillMaxSize()
-            ) {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .fillMaxHeight()
-                        .weight(0.8f)
-                ) {
-                    val painter = context.resources.getIdentifier(
-                        service.image,
-                        "drawable",
-                        context.packageName
-                    )
-                    Image(
-                        painter = painterResource(painter),
-                        contentScale = ContentScale.Fit,
-                        modifier = Modifier
-                            .align(Alignment.CenterHorizontally)
-                            .requiredSize(180.dp), contentDescription = ""
-                    )
-                    Text(
-                        text = service.name,
-                        style = MaterialTheme.typography.titleLarge,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
+        Column(modifier = Modifier.clickable(onClick = { }), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceBetween) {
+            var painter = context.resources.getIdentifier(
+                service.image_name,
+                "drawable",
+                context.packageName
+            )
+            if(painter == 0){
+                painter = R.drawable.logo_plomeria
+            }
+            Image(
+                painter = painterResource(painter),
+                contentDescription = null, // decorative
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .height(150.dp)
+                    .fillMaxWidth()
+            )
+            Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                Text(
+                    service.name,
+                    Modifier.padding(10.dp),
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleLarge,
+                )
             }
         }
     }
